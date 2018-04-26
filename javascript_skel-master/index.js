ce7bb3a158430f38122c4cb11a5977c992e3c4f9
Manaf Alajmi <Manaf.alajmi@gmail.com>
2018-04-26 13:14:18 -0700

    adding first tests

--- javascript_skel-master/index.js
+++ javascript_skel-master/index.js
@@ -1,7 +1,7 @@
-function foo() {
+function IntRoman() {
     return true
 }
 
 module.exports = {
-    foo
+    IntRoman
 }
--- javascript_skel-master/index.spec.js
+++ javascript_skel-master/index.spec.js
@@ -1,8 +1,27 @@
-const { foo } = require('./index')
+const { IntRoman } = require('./index')
 const equal = require('assert').deepEqual
 
-describe('', () => {
-    it('', () => {
-        equal(foo(), true)
+describe('Passing in 1', () => {
+    it('What is it?', () => {
+        equal(IntRoman(1), "I")
     })
+})
+
+describe('passing in 2', () => {
+    it('What is it?', () => {
+        equal(IntRoman(2), "II")
+    })
+})
+
+describe('passing in 3', () => {
+    it('What is it?', () => {
+        equal(IntRoman(3), "III")
+    })
+})
+
+describe('passing in 4', () => {
+    it('What is it?', () => {
+        equal(IntRoman(4), "IV")
+    })
+
 })
