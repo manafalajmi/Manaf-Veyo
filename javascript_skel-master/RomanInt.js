b42ce8c0dc145af0e7e9f3861f1642c5afe34ed5
Manaf Alajmi <Manaf.alajmi@gmail.com>
2018-04-29 05:10:50 -0700

    comit

--- javascript_skel-master/RomanInt.js
+++ javascript_skel-master/RomanInt.js
@@ -1,11 +1,27 @@
 function RomanInt(Numeral) {
 
+    var size = [1, 5, 10, 50, 100, 500, 1000]
+    var Numerals = ["I", "V", "X", "L", "C", "D", "M"]
+    var word = new String(Numeral)
 
-    if (Numeral == "I")
-        return 1;
+    var myMap = new Map();
 
+    var number = 0;
+    //checks to add, 
+    //everything in string should be in the map
+    //everything in string should be caps
+    //value passed in should be a string
 
-    return true
+    for (i = 0; i < size.length; i++) {
+        myMap.set(Numeral[i], size[i]);
+    }
+
+    for (i = 0; i < Numeral.length; i++) {
+        number += myMap.get(Numeral.charAt(i));
+
+    }
+
+    return number
 }
 
 
--- javascript_skel-master/RomanInt.spec.js
+++ javascript_skel-master/RomanInt.spec.js
@@ -1,8 +1,29 @@
 const { RomanInt } = require('./RomanInt')
 const equal = require('assert').deepEqual
 
-describe('passing in', () => {
+describe('passing in 1', () => {
     it('What is it?', () => {
-        equal(RomanInt("I"), "1")
+        equal(RomanInt("I"), 1)
+    })
+})
+
+describe('passing in ', () => {
+    it('What is it?', () => {
+        equal(RomanInt("II"), 2)
+    })
+})
+describe('passing in 3', () => {
+    it('What is it?', () => {
+        equal(RomanInt("III"), 3)
+    })
+})
+describe('passing in 4', () => {
+    it('What is it?', () => {
+        equal(RomanInt("IV"), 4)
+    })
+})
+describe('passing in 5', () => {
+    it('What is it?', () => {
+        equal(RomanInt("V"), 5)
     })
 })
